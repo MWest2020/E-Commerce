f0fb0dcd298d0afaa799aae1feb49b988e71254f
MWest2020 <mwesterweel@hotmail.com>
2021-03-09 15:00:58 +0100

    start of week

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,11 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+export default function PageNotFound() {
+  return (
+    <>
+      <p>Page Not Found </p>
+      <Link to="/"> Home</Link>
+    </>
+  );
+}
